6bad17a236f0df225ec142bd94220a936caa3a02
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-08-04 10:45:10 +0300

    Login component and function was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from './_services/authService/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    this.navigateUserToStartPage();
+  }
+
+  constructor(private authService: AuthServiceService, private router: Router) {       
+  }
+
   title = 'DockerJenkinsAngular';
+
+  private navigateUserToStartPage(){
+    let isUserLoggedIn = this.authService.isUserLogged()
+
+    if (isUserLoggedIn) this.router.navigate(['home']);
+    else this.router.navigate(['login']);
+  }
+
 }
