6faf4a792e55fd87f5a8aa3659fa9b0b40f1187b
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-10-17 15:24:31 +0300

    Debuging tests

--- hubHealthCheck.sh
+++ hubHealthCheck.sh
@@ -1,6 +1,6 @@
 echo "Checking if Selenium Hub is ready"
 
-while [ "$( curl -s http://HUB:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+while [ "$( curl -s http://hub:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
 	sleep 1
 done
