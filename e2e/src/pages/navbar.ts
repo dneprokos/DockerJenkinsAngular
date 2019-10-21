e652c73a3dbd7382d10213b780f58b6679583b84
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-10-21 13:28:32 +0300

    Fixed navbar menu issue. Test is passing now

--- e2e/src/pages/navbar.ts
+++ e2e/src/pages/navbar.ts
@@ -1,14 +1,12 @@
-import { element, by, browser } from "protractor";
+import { element, by, browser } from 'protractor';
 
 export class Navbar {
-    private adminMenu = element(by.xpath(`//button[@class = 'mat-icon-button']`)); 
+    private adminMenu = element(by.id('appMenu'));
     private logoutBtn = element(by.id('signOutBtn'));
 
-    clickLogoutBtn(){
+    clickLogoutBtn() {
         this.adminMenu.click();
         browser.sleep(1000);
         this.logoutBtn.click();
     }
-
-    
 }
