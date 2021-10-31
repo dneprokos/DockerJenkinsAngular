07643ba19a2ea44461f80ec1bb252a0d7d6e1577
Kostiantyn Teltov <dneprokos@gmail.com>
2021-10-31 09:49:20 +0200

    Moved to latest release version of Angular - 12.2.12

--- src/app/_core/material.module.ts
+++ src/app/_core/material.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from "@angular/core";
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
-  MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule, MatSidenavModule,
-  MatListModule, MatGridListModule, MatTooltipModule
-} from '@angular/material';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
