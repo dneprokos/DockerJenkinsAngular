4f08fd88780c7bb1728766c71bf238afa92d0747
Kostiantyn Teltov <dneprokos@gmail.com>
2021-11-01 10:42:55 +0200

    Date-pickers page was added. Fixed styles

--- src/app/_components/date-pickers/date-pickers.component.ts
+++ src/app/_components/date-pickers/date-pickers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-date-pickers',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-pickers.component.css']
 })
 export class DatePickersComponent implements OnInit {
+  campaignOne: FormGroup;
+  campaignTwo: FormGroup;
 
-  constructor() { }
+  constructor() {
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
+    this.campaignOne = new FormGroup({
+      start: new FormControl(new Date(year, month, 13)),
+      end: new FormControl(new Date(year, month, 16))
+    });
+
+    this.campaignTwo = new FormGroup({
+      start: new FormControl(new Date(year, month, 15)),
+      end: new FormControl(new Date(year, month, 19))
+    });
+  }
 
   ngOnInit() {
   }
---
+++ src/app/_components/end-to-end/end-to-end.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-to-end',
+  templateUrl: './end-to-end.component.html',
+  styleUrls: ['./end-to-end.component.css']
+})
+export class EndToEndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
